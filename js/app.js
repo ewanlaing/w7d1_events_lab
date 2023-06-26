09b162c45261c969e56eb803c3302a4707cb462c
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-06-26 15:44:21 +0100

    all done

---
+++ js/app.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const handleForSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target.title.value);
+    console.log(event.target.author.value);
+    console.log(event.target.category.value);
+
+    const title = document.createElement('h1');
+    title.textContent = `${event.target.title.value}`;
+
+    const author = document.createElement('h2');
+    author.textContent = `${event.target.author.value}`;
+
+    const category = document.createElement('p');
+    category.textContent = `${event.target.category.value}`;
+
+
+    const newBook = document.createElement('ul');
+    newBook.appendChild(title);
+    newBook.appendChild(author);
+    newBook.appendChild(category);
+
+    const list = document.querySelector('#reading-list');
+    list.appendChild(newBook);
+
+    form.reset();
+
+  }
+
+  const handleForDelete = (event) => {
+    event.preventDefault();
+    const list = document.querySelector('ul');
+    list.innerHTML = '';
+  }
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleForSubmit);
+
+  const remove = document.querySelector('#delete');
+  remove.addEventListener('submit', handleForDelete);
+
+})
